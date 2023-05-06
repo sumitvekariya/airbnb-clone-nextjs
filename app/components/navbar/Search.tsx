85e119729a22ab69f28cdcc87e84b6f98e7df28a
sumitvekariya <sumitvekariya7@gmail.com>
2023-05-06 10:58:53 +0530

    loader, error & route gurad

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,13 +1,56 @@
-'use client'
+'use client';
+import useCountries from '@/app/hooks/useCountries';
 import useSearchModal from '@/app/hooks/useSearchModal';
+import { differenceInDays } from 'date-fns';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 const Search = () => {
-    const searchModal = useSearchModal();
-    return (
-        <div
-            onClick={searchModal.onOpen}
-            className='
+  const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week';
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
+  return (
+    <div
+      onClick={searchModal.onOpen}
+      className='
             border-[1px]
             w-full
             md:w-auto
@@ -18,26 +61,26 @@ const Search = () => {
             transition
             cursor-pointer
         '
-        >
-            <div
-                className="
+    >
+      <div
+        className='
                 flex
                 flex-row
                 items-center
                 justify-between
-            "
-            >
-                <div
-                    className="
+            '
+      >
+        <div
+          className='
                     text-sm
                     font-semibold
                     px-6
-                "
-                >
-                    Anywhere
-                </div>
-                <div
-                    className="
+                '
+        >
+          {locationLabel}
+        </div>
+        <div
+          className='
                     hidden
                     sm:block
                     text-sm
@@ -46,12 +89,12 @@ const Search = () => {
                     border-x-[1px]
                     flex-1
                     text-center
-                "
-                >
-                    Any Week
-                </div>
-                <div
-                    className="
+                '
+        >
+          {durationLabel}
+        </div>
+        <div
+          className='
                     text-sm
                     pl-6
                     pr-2
@@ -60,34 +103,30 @@ const Search = () => {
                     flex-row
                     items-center
                     gap-3
-                "
-                >
-                    <div
-                        className="
+                '
+        >
+          <div
+            className='
                         hidden
                         sm:block
-                    "
-                    >
-                        Add Guests
-                    </div>
-                    <div
-                        className="
+                    '
+          >
+            {guestLabel}
+          </div>
+          <div
+            className='
                             p-2
                             bg-rose-500
                             rounded-full
                             text-white
-                        "
-                    >
-                        <BiSearch></BiSearch>
-                        
-                    </div>
-
-                </div>
-
-            </div>
-
+                        '
+          >
+            <BiSearch></BiSearch>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
